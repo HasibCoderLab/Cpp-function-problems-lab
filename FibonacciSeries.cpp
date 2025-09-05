74880936587ebdf39eeffac05b7d152ad34fcff0
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-05 23:02:56 +0600

    create a file for solve problem

--- FibonacciSeries.cpp
+++ FibonacciSeries.cpp
@@ -4,24 +4,25 @@
 //               Topic   : [Function]
 //               Problem : 👉👉 [4]
 //               Want    : FibonacciSeries.cpp
-//                        
+//
 // ================================================
 #include <iostream>
 using namespace std;
-void printFibonacci(int n){
-    int a =0;
-    int b =1;
-    for (int i = 0; i <=n; i++)
+void printFibonacci(int n)
+{
+    int a = 0;
+    int b = 1;
+    for (int i = 0; i <= n; i++)
     {
-       int next =a;
-       a = b ;
-       b = next ;
+        int next = a;
+        a = b;
+        b = next;
     }
-    
 }
-int main(){
+int main()
+{
     int n;
-    cout<< "Enter number " ;
-    cin>> n;
-    return 0 ;
+    cout << "Enter number ";
+    cin >> n;
+    return 0;
 }
