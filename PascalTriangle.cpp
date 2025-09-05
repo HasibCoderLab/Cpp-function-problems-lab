254855543fd64ea9e5a123a384d328b61089ce6c
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-05 22:51:07 +0600

    solve

--- PascalTriangle.cpp
+++ PascalTriangle.cpp
@@ -9,7 +9,7 @@
 using namespace std;
 int fact(int a ){
     int f = 1 ;
-    for (int i = 1; i <a; i++)
+    for (int i = 1; i <=a; i++)
     {
         f*=i ;
        
@@ -19,22 +19,22 @@ int fact(int a ){
 }
 // nCr Function
 int nCr(int n , int r ){
-    return fact(n)/ (fact(n)* fact(r)) ;
+    return fact(n)/ (fact(r)* fact(n-r)) ;
 
 }
 int main(){
     int n;
     cout<< "Enter number " ;
     cin>> n;
-    for (int i = 1; i <=n; i++)
+    for (int i = 0; i < n; i++)
     {
         // print space .
-        for (int  j = 1; j <= n- i-j ; j++)
+        for (int  j = 0; j < n- i-1 ; j++)
         {
            cout<<" " ;
         }
         // Prinit Numbers ;
-        for (int  j = 1;j <=i; j++)
+        for (int  j = 0;j <=i; j++)
         {
             cout<<nCr(i,j) <<" ";
         }
