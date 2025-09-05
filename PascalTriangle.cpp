535c18290820885c30a9724fbce88c151331e1cb
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-05 22:37:38 +0600

    solve 3 th problem of 'Function' in C++

--- PascalTriangle.cpp
+++ PascalTriangle.cpp
@@ -1,15 +1,47 @@
 // ================================================
 //               Language: C++ / Cpp
 //               Topic   : [Function]
-//               Problem : 👉👉 [1]
+//               Problem : 👉👉 [3]
 //               Want    : PascalTriangle
 //                        
 // ================================================
 #include <iostream>
 using namespace std;
+int fact(int a ){
+    int f = 1 ;
+    for (int i = 1; i <a; i++)
+    {
+        f*=i ;
+       
+    }
+    return f;
+    
+}
+// nCr Function
+int nCr(int n , int r ){
+    return fact(n)/ (fact(n)* fact(r)) ;
+
+}
 int main(){
     int n;
     cout<< "Enter number " ;
     cin>> n;
+    for (int i = 1; i <=n; i++)
+    {
+        // print space .
+        for (int  j = 1; j <= n- i-j ; j++)
+        {
+           cout<<" " ;
+        }
+        // Prinit Numbers ;
+        for (int  j = 1;j <=i; j++)
+        {
+            cout<<nCr(i,j) <<" ";
+        }
+        cout<<endl;
+        
+    }
+    
+
     return 0 ;
 }
