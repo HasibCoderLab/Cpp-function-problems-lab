0fcf8ae9da4f7a874a30a33508580d3e9aaeabb9
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-05 21:26:00 +0600

    create a file for solve problem

---
+++ PascalTriangle.cpp
@@ -0,0 +1,15 @@
+// ================================================
+//               Language: C++ / Cpp
+//               Topic   : [Function]
+//               Problem : 👉👉 [1]
+//               Want    : PascalTriangle
+//                        
+// ================================================
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<< "Enter number " ;
+    cin>> n;
+    return 0 ;
+}
